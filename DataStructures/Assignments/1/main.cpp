171977f7747fba5e083f8455035015465fe2a515
Ryan Brosnahan <BrosnahanRyan@gmail.com>
2012-09-16 16:48:11 -0400

    DS assignment 1

--- DataStructures/Assignments/1/main.cpp
+++ DataStructures/Assignments/1/main.cpp
@@ -6,6 +6,9 @@
 #include "ListQue.cpp"
 #include "ListStack.cpp"
 
+
+int length(ListQue<char> &);
+
 int main() {
 
 	/*
@@ -65,13 +68,73 @@ int main() {
         //this logic stops checking at the first instance of failure
 		if( q.pop() == s.pop() )
             continue;
+
         pal = 0;
         break;
 	}
 
     //print the results
-    std::cout << (pal ? "Succes!" : "Failure!") << std::endl;
+    std::cout << (pal ? "That was a palindrome!" : "That was not a palindrome!") << std::endl;
+
+
+    /*
+    Question 3 Implimentation
+    Function is below
+     */
+
+    ListQue<char> q3();
+
+    for (int i = 0; i < rand() % 100; i++)
+    	q3.push(rand() % 100);
+
+    std::cout << "Length of randomly generated queue for question 3: " <<
+    			length(q3);
+
 
     std::cin.get();
 	return 0;
+}
+
+
+
+/*
+Question 3
+Implement a client function that returns the number of elements in a queue
+We must assume the queue has functions:
+	pop
+	push
+	isempty
+We also assume it is a list of chars
+
+What would the time requirements be in BigO?
+
+To answer this we must assume that the queue operates in lenear time.
+Time requirement:
+	n for each pop
+	n for each length++
+	n for each push
+
+	3+3n
+
+	BigO(n)
+
+ */
+
+int Length(ListQue<char>& queue) {
+
+	ListQue<char> tempQ;
+	int length = 0;
+
+	//empty the queue while counting the elements
+	while(!queue.isempty()) {
+		tempQ.push(queue.pop());
+		length++;
+	}
+
+	//refill the queue from the temporary one
+	while(!queue.isempty())
+		queue.push(tempQ.pop());
+
+	return length;
+
 }
