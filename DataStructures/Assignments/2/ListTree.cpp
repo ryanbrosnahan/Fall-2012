f0844c8bab54a30be09d08b2d9d6a5ef8689d66f
Ryan Brosnahan <BrosnahanRyan@gmail.com>
2012-10-03 01:25:06 -0400

    finished with DS assignment 2

--- DataStructures/Assignments/2/ListTree.cpp
+++ DataStructures/Assignments/2/ListTree.cpp
@@ -25,18 +25,35 @@ class ListTree {
 
 	// Destructor
 	~ListTree();
+	void deleteTreeNode(TreeNode<T> *&);
 
 
 	T sum(TreeNode<T> *);
+	T sumAll();
 
 
 	void display();
+	void displayLess(T);
+	void displayLessHidden(TreeNode<T> *, T);
 	void displayPre(TreeNode<T> *);
 
 	void insert(T);
-	void addTreeNode(TreeNode<T> *, T);
+	void addTreeNode(TreeNode<T> *&, T);
 
-	void deleteTreeNode(T);
+
+	int maxDepth();
+	int maxDepthHidden(TreeNode<T> *);
+	int minDepth();
+	int minDepthHidden(TreeNode<T> *);
+
+	bool isBalanced();
+
+	int nodeDepth(T );
+	int nodeDepthHidden(TreeNode<T> *, T, int);
+
+	int maxPath();
+	int maxPathHidden(TreeNode<T> *, int);
+
 
 };
 
@@ -52,13 +69,43 @@ ListTree<T>::ListTree(T val) {
 
 template <typename T>
 ListTree<T>::~ListTree() {
+	deleteTreeNode(head);
+}
 
+template <typename T>
+void ListTree<T>::deleteTreeNode(TreeNode<T> *&node) {
+	if(node != NULL) {
+		deleteTreeNode(node->left);
+		deleteTreeNode(node->right);
+		delete node;
+	}
+
+	node = NULL;
 }
 
 template <typename T>
 void ListTree<T>::display() {
 	displayPre(head);
-	std::cout << "test1";
+
+}
+
+template <typename T>
+void ListTree<T>::displayLess(T val) {
+	std::cout << "Print all nodes less than " << val << ": ";
+	displayLessHidden(head, val);
+}
+
+template <typename T>
+void ListTree<T>::displayLessHidden(TreeNode<T> *node, T val) {
+
+   if (node != NULL) {
+      displayLessHidden(node->left, val);
+
+      if (node->value < val)
+         cout << node->value << " ";
+
+      displayLessHidden(node->right, val);
+   }
 }
 
 template <typename T>
@@ -67,20 +114,17 @@ void ListTree<T>::displayPre(TreeNode<T> *node) {
 		cout << node->value << " ";
 		displayPre(node->left);
 		displayPre(node->right);
-			std::cout << "test2";
 	}
-		std::cout << "test3";
 }
 
 template <typename T>
 void ListTree<T>::insert(T val) {
-
 	addTreeNode(head, val);
 	return;
 }
 
 template <typename T>
-void ListTree<T>::addTreeNode(TreeNode<T> *node, T val) {
+void ListTree<T>::addTreeNode(TreeNode<T> *&node, T val) {
 	if(node == NULL)
 		node = new TreeNode<T>(val);
 	else if(val < node->value)
@@ -90,11 +134,96 @@ void ListTree<T>::addTreeNode(TreeNode<T> *node, T val) {
 	return;
 }
 
+
+template <typename T>
+T ListTree<T>::sumAll() {
+	return sum(head);
+}
+
 template <typename T>
 T ListTree<T>::sum(TreeNode<T> *node) {
-	if(node->value == NULL)
-		return NULL;
+	if(node == NULL)
+		return 0;
 
-	std::cout << node->value << std::endl;
 	return (node->value + sum(node->left) + sum(node->right));
 }
+
+template <typename T>
+int ListTree<T>::maxDepth() {
+	maxDepthHidden(head);
+}
+
+template <typename T>
+int ListTree<T>::maxDepthHidden(TreeNode<T> *node) {
+	if(node == NULL)
+		return 0;
+
+	int left = maxDepthHidden(node->left);
+	int right = maxDepthHidden(node->right);
+
+	if(left > right)
+		return(left+1);
+	else
+		return (right+1);
+}
+
+template <typename T>
+int ListTree<T>::minDepth() {
+	minDepthHidden(head);
+}
+
+template <typename T>
+int ListTree<T>::minDepthHidden(TreeNode<T> *node) {
+	if(node == NULL)
+		return 0;
+
+	int left = minDepthHidden(node->left);
+	int right = minDepthHidden(node->right);
+
+	if(left < right)
+		return(left+1);
+	else
+		return (right+1);
+}
+
+template <typename T>
+bool ListTree<T>::isBalanced() {
+	if((maxDepth() - minDepth()) <= 1)
+		return 1;
+
+	return 0;
+}
+
+template <typename T>
+int ListTree<T>::nodeDepth(T value) {
+    return nodeDepthHidden(head, value, 1);
+}
+
+template <typename T>
+int ListTree<T>::nodeDepthHidden(TreeNode<T> *node, T value, int level) {
+    if (node == NULL)
+       return 0;
+
+    if (node->value == value)
+       return level - 1;
+
+    return max(nodeDepthHidden(node->left, value, level+1), nodeDepthHidden(node->right, value, level+1));
+}
+
+
+
+template <typename T>
+int ListTree<T>::maxPath() {
+	return maxPathHidden(head, 0);
+}
+
+template <typename T>
+int ListTree<T>::maxPathHidden(TreeNode<T> *node, int total){
+	if(node == NULL)
+		return 0;
+
+	return total + node->value + max(maxPathHidden(node->left, total), maxPathHidden(node->right, total));
+
+}
+
+
--- DataStructures/Assignments/2/main.cpp
+++ DataStructures/Assignments/2/main.cpp
@@ -1,11 +1,36 @@
 #include "ListTree.cpp"
+#include <time.h>
 
 int main() {
 	ListTree<int> tree;
+    srand( time(NULL) );
 
-    for(int i = 1; i<10; ++i)
-        tree.insert(i);
-
+    int sum = 0;
+    std::cout << "Randomly generated numbers to be put into a tree: ";
+    for(int i = 0; i < 3; i++) {
+        int p = rand() % 8;
+        tree.insert(p);
+        std::cout << p << " ";
+        sum += p;
+    }
+    std::cout << std::endl << "Tree contents printed in PREORDER: ";
     tree.display();
 
+    std::cout << std::endl << "The sum of the tree should be " << sum << std::endl;
+    std::cout << "The calculated sum is " << tree.sumAll() <<std::endl;
+
+    tree.displayLess(4);
+
+
+    std::cout << std::endl;
+    std::cout << "The max depth of this tree is: " << tree.maxDepth() << std::endl;
+    std::cout << "The min depth of this tree is: " << tree.minDepth() << std::endl;
+
+    std::cout << "Is the tree balanced? " << (tree.isBalanced() ? "It is!" : "It isn't!")
+                << std::endl;
+
+    std::cout << "the depth to reach the node with value of 5 is: " << tree.nodeDepth(5)
+                << std::endl;
+
+    std::cout << "The most expenive path costs: " << tree.maxPath();
 }
